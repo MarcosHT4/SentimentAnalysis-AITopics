895ea80e72a60e1b4aa0546a2c012cf55a6aaaa9
Marcos <marcosht503@gmail.com>
2023-12-04 14:45:07 -0400

    Added ISO 8859-1 to CSV reader

--- src/routers/reports.py
+++ src/routers/reports.py
@@ -15,6 +15,6 @@ def get_csv_reports() -> Response:
     csv_file_path = SETTINGS.csv_path
     if not os.path.exists(csv_file_path):
         raise HTTPException(status_code=404, detail="CSV File not found")
-    with open(csv_file_path, "r") as f:
+    with open(csv_file_path, "r", encoding="ISO-8859-1") as f:
         csv_content = f.read()
     return Response(content=csv_content, media_type="text/csv")
